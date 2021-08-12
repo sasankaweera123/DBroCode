c59359c12082addfdb0001de089145f1820f760e
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2021-08-12 08:37:28 +0530

    Created New Design (Design 04)

---
+++ Design 04/Js/main.js
@@ -0,0 +1,27 @@
+const countdown = () => {
+    const countDate = new Date("August 17, 2021 00:00:00").getTime();
+
+    const currentTime = new Date().getTime();
+
+    const gap = countDate - currentTime;
+
+    const millisecond = 1;
+    const second = millisecond * 1000;
+    const minutes = second * 60;
+    const hour = minutes * 60;
+    const day = hour * 24;
+
+
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinutes = Math.floor((gap % hour) / minutes);
+    const textSecond = Math.floor((gap % minutes) / second);
+    const textMillisecond = Math.floor((gap % second) / millisecond);
+
+    document.querySelector('.hour').innerText = textHour;
+    document.querySelector('.minutes').innerText = textMinutes;
+    document.querySelector('.seconds').innerText = textSecond;
+    document.querySelector('.miliSeconds').innerText = textMillisecond;
+
+};
+
+setInterval(countdown, 250);
